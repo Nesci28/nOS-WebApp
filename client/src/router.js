d83acf97c497e1d46ba7469ca0642332cac2b9a4
Marc Gagnon <markgagnon28@gmail.com>
2019-02-25 19:41:13 -0500

    system config

--- client/src/router.js
+++ client/src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import User from './views/User.vue';
+import Example from './views/Example.vue';
+import System from './views/System.vue';
 
 Vue.use(Router);
 
@@ -13,9 +14,14 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/user',
-      name: 'user',
-      component: User,
+      path: '/example',
+      name: 'example',
+      component: Example,
+    },
+    {
+      path: '/System/:id',
+      name: 'System',
+      component: System,
     }
   ],
 });
