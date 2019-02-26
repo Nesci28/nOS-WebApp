95094eb82300002c1ae942fcfe8c3471330ecad4
Marc Gagnon <markgagnon28@gmail.com>
2019-02-25 19:41:13 -0500

    system config

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,29 +3,31 @@
     background: -webkit-linear-gradient(to top, #000000, #4b0082, #669999);
     background: linear-gradient(to top, #000000, #4b0082, #669999);
   ">
-    <h1 class="loginText pa-2">Login</h1>
+    <h3 class="loginText pt-2">Use the login information you rigs are setup with.</h3>
+    <h3 class="loginText pb-2">You can enter with "admin" in both fields to view an exemple.</h3>
     <v-layout justify-center>
       <v-card class="textField elevation-20" color="transparent" height="100%" fill-height>
-      <v-form @submit.prevent="checkLogin" color="transparent">
-      <v-text-field
-        v-model="user.login"
-        color="white"
-        label="Username"
-        placeholder="email address"
-      ></v-text-field>
-      <v-text-field
-        v-model="user.password"
-        color="white"
-        label="Password"
-        :type="'password'"
-      ></v-text-field>
-      <div>
-        <v-btn class="formBtn" @click="clear()" color="white">Clear</v-btn>
-        <v-btn class="formBtn" type="submit" color="success">Submit</v-btn>
-      </div>
-      </v-form>
+        <v-form @submit="checkLogin" color="transparent">
+          <v-text-field
+            v-model="user.login"
+            color="white"
+            label="Username"
+          ></v-text-field>
+          <v-text-field
+            v-model="user.password"
+            color="white"
+            label="Password"
+            :type="'password'"
+          ></v-text-field>
+          <div>
+            <v-btn class="formBtn" @click="clear()" color="white">Clear</v-btn>
+            <v-btn class="formBtn" type="submit" color="success">Submit</v-btn>
+          </div>
+        </v-form>
       </v-card>
     </v-layout>
+    <div>
+  </div>
   </v-app>
 </template>
 
@@ -45,7 +47,7 @@ export default {
     checkLogin() {
       if (this.user.login == 'admin' && this.user.password == 'admin') {
         this.access = "true"
-        this.$router.push('user')
+        this.$router.push('example')
       } else {
         this.access = "false"
       }
---
+++ client/src/views/System.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-app style="
+    background: -webkit-linear-gradient(to top, #000000, #4b0082, #669999);
+    background: linear-gradient(to top, #000000, #4b0082, #669999);
+  ">
+  <h1 class="centerText pa-2">System Config {{ id }}</h1>
+    <v-container grid-list-md text-xs-center>
+      <v-layout row wrap>
+        <v-flex xs2 pt-4>
+          <h3>Nvidia Coin :</h3>
+        </v-flex>
+        <v-flex xs2>
+          <v-text-field v-model="coin" label="" color="white"></v-text-field>
+        </v-flex>
+        <v-flex xs2 pt-4>
+          <h3>Amd Coin :</h3>
+        </v-flex>
+        <v-flex xs2>
+          <v-text-field v-model="coin" color="white" label=""></v-text-field>
+        </v-flex>
+        <v-flex xs2 pt-4>
+          <h3>CPU Coin :</h3>
+        </v-flex>
+        <v-flex xs2>
+          <v-text-field v-model="coin" color="white" label=""></v-text-field>
+        </v-flex>
+        <v-flex xs2 pt-4>
+          <h3>Wifi SSID :</h3>
+        </v-flex>
+        <v-flex xs2>
+          <v-text-field v-model="wifiSsid" color="white" label=""></v-text-field>
+        </v-flex>
+        <v-flex xs2 pt-4>
+          <h3>Wifi Pass :</h3>
+        </v-flex>
+        <v-flex xs2>
+          <v-text-field v-model="wifiPassword" color="white" label=""></v-text-field>
+        </v-flex>
+        <v-flex xs2>
+        </v-flex>
+        <v-flex xs2>
+        </v-flex>
+        <v-flex xs2 pt-4>
+          <h3>Donation :</h3>
+        </v-flex>
+        <v-flex xs6>
+          <v-text-field mask="###" outline color="white" v-model="donation" label="Numbers only"></v-text-field>
+        </v-flex>
+        <v-flex xs4>
+        </v-flex>
+        <v-flex xs6 sm3 md2 lg2>
+          <v-checkbox color="white" label="SSH"></v-checkbox>
+        </v-flex>
+        <v-flex xs6 sm3 md2 lg2>
+          <v-checkbox color="white" label="Teamviewer"></v-checkbox>
+        </v-flex>
+        <v-flex xs6 sm3 md2 lg2>
+          <v-checkbox color="white" label="Anydesk"></v-checkbox>
+        </v-flex>
+        <v-flex xs6 sm3 md2 lg2>
+          <v-checkbox color="white" label="Gpu light"></v-checkbox>
+        </v-flex>
+        <v-flex xs6 sm3 md2 lg2>
+          <v-checkbox color="white" label="EthPill"></v-checkbox>
+        </v-flex>
+        <v-flex xs6 sm3 md2 lg2>
+          <v-checkbox color="white" label="Auto-OC"></v-checkbox>
+        </v-flex>
+        <v-flex xs6 sm3 md2 lg2>
+          <v-checkbox color="white" label="WatchDog"></v-checkbox>
+        </v-flex>
+      </v-layout>
+      <div>
+        <v-btn class="right" color="success">Save</v-btn>
+        <v-btn class="right" color="white">Defaults</v-btn>
+      </div>
+    </v-container>
+    <v-container>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      id: 0
+    };
+  },
+  methods: {
+
+  },
+  created() {
+     this.id = this.$route.params.id;
+  }
+};
+</script>
+
+<style>
+.right{
+  right: -5%;
+}
+.centerText{
+  text-align: center;
+}
+</style>
+
