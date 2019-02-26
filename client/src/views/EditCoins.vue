e6d96786dbb47e25a2afaa5491acfb35e0d96a02
Marc Gagnon <markgagnon28@gmail.com>
2019-02-26 13:30:21 -0500

    added config pages, changed the theme a bit, updated System Config

---
+++ client/src/views/EditCoins.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <div class="background">
+      
+      <h1 class="white--text centerText pa-2">Coins Configurator {{ id.toUpperCase() }}</h1>
+        
+      </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      id: 0,
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  created() {
+    this.id = this.$route.params.id;
+  }
+};
+</script>
+
+<style>
+.background{
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  background: url(../assets/btc.jpeg);
+  height: 100%;
+  background-size: cover;
+}
+.centerText{
+  text-align: center;
+  text-decoration: underline;
+}
+</style>
+
